08c3052a7ca947ef583e456dc92484e9a7841b27
lauren brett <laurencareybrett@gmail.com>
2020-02-25 10:53:32 +0000

    set up ministry of magic

---
+++ src/test/java/com/codeclan/example/MinistryOfMagic/MinistryOfMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.MinistryOfMagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistryOfMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
